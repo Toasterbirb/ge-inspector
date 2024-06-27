56ae608a73dfb54f149439af66e3f9f3871a7d93
Toasterbirb <git.53jvg@aleeas.com>
2024-06-28 01:05:38 +0300

    Simplify the range values into a struct

--- src/main.cpp
+++ src/main.cpp
@@ -23,6 +23,12 @@ int main(int argc, char** argv)
 		f2p, p2p, all
 	};
 
+	struct range
+	{
+		i64 min = 0;
+		i64 max = std::numeric_limits<i64>::max();
+	};
+
 	member_filter member_filter = member_filter::all;
 
 	bool update_db = false;
@@ -35,20 +41,11 @@ int main(int argc, char** argv)
 	bool print_index = false;
 	std::string regex_pattern;
 
-	i64 min_price = 0;
-	i64 max_price = std::numeric_limits<i64>::max();
-	i64 budget = std::numeric_limits<i64>::max();
+	// Filtering ranges
+	range price, volume, limit, alch, cost;
 
-	i64 min_volume = 1;
-	i64 max_volume = std::numeric_limits<i64>::max();
-
-	i64 min_limit = 0;
-	i64 max_limit = std::numeric_limits<i64>::max();
-
-	i64 min_alch = 0;
-	i64 max_alch = std::numeric_limits<i64>::max();
-
-	i64 min_cost = 0;
+	// Set default range values
+	volume.min = 1;
 
 	std::string name_contains;
 
@@ -79,16 +76,16 @@ int main(int argc, char** argv)
 		clipp::option("--index").set(print_index) % "print the indices of items",
 		(clipp::option("--name", "-n") & clipp::value("str", name_contains)) % "filter items by name",
 		(clipp::option("--regex") & clipp::value("pattern", regex_pattern)) % "filter items by name with regex",
-		(clipp::option("--min-price") & clipp::value("price", min_price)) % "minimum price",
-		(clipp::option("--max-price") & clipp::value("price", max_price)) % "maximum price",
-		(clipp::option("--min-volume") & clipp::value("volume", min_volume)) % "minimum volume (def: 1)",
-		(clipp::option("--max-volume") & clipp::value("volume", max_volume)) % "maximum volume",
-		(clipp::option("--min-limit") & clipp::value("limit", min_limit)) % "minimum buy limit",
-		(clipp::option("--max-limit") & clipp::value("limit", max_limit)) % "maximum buy limit",
-		(clipp::option("--min-alch") & clipp::value("alch", min_alch)) % "minimum high alchemy amount",
-		(clipp::option("--max-alch") & clipp::value("alch", max_alch)) % "maximum high alchemy amount",
-		(clipp::option("--min-cost") & clipp::value("cost", min_cost)) % "minimum cost of the flip",
-		(clipp::option("--budget", "-b") & clipp::value("budget", budget)) % "maximum budget for total cost",
+		(clipp::option("--min-price") & clipp::value("price", price.min)) % "minimum price",
+		(clipp::option("--max-price") & clipp::value("price", price.max)) % "maximum price",
+		(clipp::option("--min-volume") & clipp::value("volume", volume.min)) % "minimum volume (def: 1)",
+		(clipp::option("--max-volume") & clipp::value("volume", volume.max)) % "maximum volume",
+		(clipp::option("--min-limit") & clipp::value("limit", limit.min)) % "minimum buy limit",
+		(clipp::option("--max-limit") & clipp::value("limit", limit.max)) % "maximum buy limit",
+		(clipp::option("--min-alch") & clipp::value("alch", alch.min)) % "minimum high alchemy amount",
+		(clipp::option("--max-alch") & clipp::value("alch", alch.max)) % "maximum high alchemy amount",
+		(clipp::option("--min-cost") & clipp::value("cost", cost.min)) % "minimum cost of the flip",
+		(clipp::option("--budget", "-b", "--max-cost") & clipp::value("budget", cost.max)) % "maximum budget for total cost",
 		(clipp::option("--sort", "-s") & clipp::one_of(sorting_mode_commands)) % "sort the results",
 		clipp::option("--invert", "-i").set(invert_sort) % "invert the result order",
 		(clipp::option("--color", "-c") & clipp::one_of(colorscheme_commands)) % "change the colorscheme of the output to something other than white"
@@ -124,16 +121,16 @@ int main(int argc, char** argv)
 	// Run the query
 	std::vector<ge::item> filtered_items;
 	std::copy_if(items.begin(), items.end(), std::back_inserter(filtered_items), [&](const ge::item& item){
-		bool generic_filters = item.price >= min_price
-					&& item.price <= max_price
-					&& (item.price * item.limit) <= budget
-					&& (item.price * item.limit) >= min_cost
-					&& item.volume >= min_volume
-					&& item.volume <= max_volume
-					&& item.limit >= min_limit
-					&& item.limit <= max_limit
-					&& item.high_alch >= min_alch
-					&& item.high_alch <= max_alch;
+		bool generic_filters = item.price >= price.min
+					&& item.price <= price.max
+					&& (item.price * item.limit) <= cost.max
+					&& (item.price * item.limit) >= cost.min
+					&& item.volume >= volume.min
+					&& item.volume <= volume.max
+					&& item.limit >= limit.min
+					&& item.limit <= limit.max
+					&& item.high_alch >= alch.min
+					&& item.high_alch <= alch.max;
 
 		bool name_filter = name_contains.empty();
 		if (!name_contains.empty())
