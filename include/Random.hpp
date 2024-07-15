3dbd09e34f997102c1ff8e3510c63597d2cc8e87
Toasterbirb <git.53jvg@aleeas.com>
2024-07-15 15:55:48 +0300

    Attempt to make the random seed slightly more random by reading more values from /dev/urandom

--- include/Random.hpp
+++ include/Random.hpp
@@ -2,7 +2,16 @@
 
 #include "Types.hpp"
 
+#include <concepts>
+
 namespace ge
 {
 	u64 random_seed();
+
+	template<typename T>
+	constexpr T combine_hashes(const T a, const T b)
+	{
+		static_assert(!std::floating_point<T>, "Floating point values cannot be used as hashes");
+		return a ^ (b << 1);
+	}
 }
--- src/random.cpp
+++ src/random.cpp
@@ -1,14 +1,29 @@
 #include "Random.hpp"
 
+#include <array>
 #include <fstream>
 
 namespace ge
 {
 	u64 random_seed()
 	{
-		size_t seed;
+		// Read in values from /dev/urandom and combine them into a singular seed
+
+		constexpr u8 seed_value_count = 4;
+
+		std::array<size_t, seed_value_count> seeds;
 		std::fstream urandom("/dev/urandom", std::ios::in | std::ios::binary);
-		urandom.read(reinterpret_cast<char*>(&seed), sizeof(seed));
+
+		// Read the values in
+		for (u8 i = 0; i < seed_value_count; ++i)
+			urandom.read(reinterpret_cast<char*>(&seeds[i]), sizeof(seeds.at(i)));
+
+		size_t seed = seeds.at(0);
+
+		// Combine everything into a singular value
+		for (u8 i = 1; i < seed_value_count; ++i)
+			seed = combine_hashes(seed, seeds.at(i));
+
 		return seed;
 	}
 }
