bc62c0e9111652474654ddac16c634346d4fe4ec
Toasterbirb <git.53jvg@aleeas.com>
2024-07-16 12:13:57 +0300

    Move braces to new lines

--- include/ItemSortLambdas.hpp
+++ include/ItemSortLambdas.hpp
@@ -4,23 +4,28 @@
 
 namespace ge
 {
-	static auto sort_by_price = [](const item& a, const item& b) {
+	static auto sort_by_price = [](const item& a, const item& b)
+	{
 		return a.price < b.price;
 	};
 
-	static auto sort_by_alch = [](const item& a, const item& b) {
+	static auto sort_by_alch = [](const item& a, const item& b)
+	{
 		return a.high_alch < b.high_alch;
 	};
 
-	static auto sort_by_volume = [](const item& a, const item& b) {
+	static auto sort_by_volume = [](const item& a, const item& b)
+	{
 		return a.volume < b.volume;
 	};
 
-	static auto sort_by_total_cost = [](const item& a, const item& b) {
+	static auto sort_by_total_cost = [](const item& a, const item& b)
+	{
 		return (a.limit * a.price) < (b.limit * b.price);
 	};
 
-	static auto sort_by_limit = [](const item& a, const item& b) {
+	static auto sort_by_limit = [](const item& a, const item& b)
+	{
 		return a.limit < b.limit;
 	};
 }
