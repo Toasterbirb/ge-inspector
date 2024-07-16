de53bf02075730999279f1a026c8fbd46b78f077
Toasterbirb <git.53jvg@aleeas.com>
2024-07-16 16:14:59 +0300

    Add compiler attributes to price utils functions

--- include/PriceUtils.hpp
+++ include/PriceUtils.hpp
@@ -6,7 +6,12 @@
 
 namespace ge
 {
+	__attribute__((warn_unused_result, const))
 	std::string round_big_numbers(const u64 value);
+
+	__attribute__((warn_unused_result, const))
 	std::string clean_decimals(const double value);
+
+	__attribute__((warn_unused_result, const))
 	u64 round(const u64 value, const i8 decimal_points);
 }
